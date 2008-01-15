7e3d915f14e26f69616a3ed41d9f3ab38dd8601f
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-01-15 13:31:55 +0000

    Still no cvs metadata !!

--- JonDonym/trunk/JAPDll/resource.h
+++
@@ -1,8 +0,0 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by JAPDll.rc
-//
-#define IDI_JAP                         101
-#define IDI_JAP_BLINK                   103
-#define IDI_WINDOW                      108
-
