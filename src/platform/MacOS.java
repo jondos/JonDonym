8ee9dd3b8ec738ade4c0a718acf0b90a5db886d4
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-04-08 13:48:37 +0000

    fixed restart for MacOS bundles

--- src/platform/MacOS.java
+++ src/platform/MacOS.java
@@ -27,9 +27,27 @@
  */
 package platform;
 
+import jap.JAPController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import anon.util.XMLParseException;
+import anon.util.XMLUtil;
+
 import logging.LogHolder;
 import logging.LogLevel;
 import logging.LogType;
+
+
 
 /**
  * This class is instantiated by AbstractOS if the current OS is MacOS or MacOSX
@@ -37,14 +55,24 @@ import logging.LogType;
 public class MacOS extends AbstractOS
 {
 	public static final String OS_NAME = "Mac OS";
-
-
+	final static String BUNDLE_CONTENTS = "Contents"+File.separator;
+	final static String BUNDLE_MAC_OS_EXECUTABLES = BUNDLE_CONTENTS+"MacOS"+File.separator;
+	final static String BUNDLE_PROPERTY_FILE_NAME = "Info.plist";
+	final static String BUNDLE_EXECUTABLE_PROPERTY_KEY = "CFBundleExecutable";
+	
+	private HashMap m_bundleProperties = null;
+	private boolean m_bundle = false;
+	private String m_bundlePath = null;
+	
 	public MacOS() throws Exception
 	{
 		if (System.getProperty("mrj.version") == null)
 		{
 			throw new Exception("Operating system is not "+ OS_NAME);
 		}
+		m_bundleProperties = new HashMap();
+		setBundlePath();
+		loadBundleProperties();
 	}
 
 	/*
@@ -53,7 +81,8 @@ public class MacOS extends AbstractOS
 		//MRJFileUtils.openURL(a_url.toString());
 		return openLink(a_url.toString());
 	}*/
-
+	
+	
 	protected boolean openLink(String a_link)
 	{
 		try
@@ -70,8 +99,6 @@ public class MacOS extends AbstractOS
 		return false;
 	}
 
-
-
 	public String getConfigPath()
 	{
 		//Return path in users's home/Library/Preferences
@@ -84,4 +111,127 @@ public class MacOS extends AbstractOS
 			return System.getProperty("user.home", "") + "/Library/Preferences/";
 		}
 	}
+	
+	/* returns absolute path to application bundle or null,
+	 * if JAP is not executed as application bundle
+	 */
+	public void setBundlePath()
+	{
+		URL url = MacOS.class.getResource(this.getClass().getSimpleName()+".class");
+		
+		if(url != null)
+		{
+			String path = url.getPath();
+			if(path != null)
+			{
+				// remove file: prefix
+				if(!(path.startsWith(File.separator)))
+				{
+					int s_index = path.indexOf("/");
+					path = (s_index != -1) ? path.substring(s_index) : path;
+				}
+						
+				LogHolder.log(LogLevel.ERR, LogType.MISC,
+						  "plg: "+path+" in MacOS default program.");
+				int index_path = path.indexOf(BUNDLE_CONTENTS);
+				if(index_path != -1)
+				{
+					/* JAP is started as an  application bundle */
+					m_bundle = true;
+					m_bundlePath = path.substring(0, index_path-1);
+					return;
+				}
+			}
+		}
+		m_bundlePath = null;
+	}
+	
+	public String getBundlePath()
+	{
+		return m_bundlePath;
+	}
+	
+	public boolean isBundle() {
+		return (m_bundlePath != null);
+	}
+	
+	/* load the bundle properties specified in the Info.plist property file */
+	protected void loadBundleProperties()
+	{
+		if(isBundle())
+		{
+			try 
+			{
+				File bundlePropertyFile = new File(m_bundlePath+File.separator+
+						BUNDLE_CONTENTS+BUNDLE_PROPERTY_FILE_NAME);
+				
+				Document bundlePropertyDoc = XMLUtil.readXMLDocument(bundlePropertyFile);
+				if(bundlePropertyDoc != null)
+				{
+					NodeList bundlePropertyDicts = bundlePropertyDoc.getElementsByTagName("dict");
+					if(bundlePropertyDicts != null)
+					{
+						int bprop_length = bundlePropertyDicts.getLength();
+						for(int index = 0; index < bprop_length; index++)
+						{
+							Node dictNode = bundlePropertyDicts.item(index);
+							if(dictNode.hasChildNodes())
+							{
+								NodeList dictChildNodes = dictNode.getChildNodes();
+								int nrChildNodes = dictChildNodes.getLength();
+								String keyName = null;
+								for (int i = 0; i < nrChildNodes; i++) {
+									Node dictChildeNode = dictChildNodes.item(i);
+									if(dictChildeNode.getNodeName().equals("key"))
+									{
+										keyName = dictChildeNode.getTextContent();
+									} 
+									else if(dictChildeNode.getNodeName().equals("string"))
+									{
+										if(keyName != null)
+										{
+											m_bundleProperties.put(keyName, dictChildeNode.getTextContent());
+											keyName = null;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			} 
+			catch (IOException ioe) 
+			{
+				LogHolder.log(LogLevel.ERR, LogType.MISC,
+						"Cannot open bundle property file: "+BUNDLE_PROPERTY_FILE_NAME+", cause:", ioe);
+			} 
+			catch (XMLParseException xpe) 
+			{
+				LogHolder.log(LogLevel.ERR, LogType.MISC,
+						"Cannot parse bundle property file: "+BUNDLE_PROPERTY_FILE_NAME+", cause:", xpe);
+			}
+		}
+	}
+	
+	/* returns absolute path to application bundle executable
+	 * which is the stub to execute the jar file
+	 * returns null, if JAP is not executed as an application bundle
+	 */
+	public String getBundleExecutablePath()
+	{
+		if(!isBundle())
+		{
+			return null;
+		}
+		String bundleExecutable = 
+			(String) m_bundleProperties.get(BUNDLE_EXECUTABLE_PROPERTY_KEY);
+		if (bundleExecutable == null)
+		{
+			return null;
+		}
+		return getBundlePath()+File.separator+BUNDLE_MAC_OS_EXECUTABLES+bundleExecutable;
+	}
+	
+	
+	
 }
