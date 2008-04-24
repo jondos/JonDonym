975c1f2fc90d8462a07f5ab550f912e039a636e9
rolf <rolf@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-04-24 07:43:52 +0000

    release -> beta

--- src/jap/JAPConstants.java
+++ src/jap/JAPConstants.java
@@ -35,7 +35,7 @@ import anon.mixminion.Mixminion;
 
 public final class JAPConstants
 {
-	public static final String aktVersion = "00.09.023"; //Never change the layout of this line!
+	public static final String aktVersion = "00.09.024"; //Never change the layout of this line!
 	private static final String CVS_GENERATED_RELEASE_DATE = "$Date: 2008-04-11 13:04:12 $";
 
 	//Warning: This is a little bit tricky,
@@ -43,7 +43,7 @@ public final class JAPConstants
 	//to the date of the last commmit of this file
 
 	//Never change the layout of the next two lines as serveal automated tools use these lines to do fancy things...
-	public final static boolean m_bReleasedVersion = true; //Set to true if this is a stable (release) Version
+	public final static boolean m_bReleasedVersion = false; //Set to true if this is a stable (release) Version
 	private static final String RELEASE_DATE = "2007/11/08 20:01:48"; // Set only to a Value, if m_bReleaseVersion=true
 
 	public static final String CURRENT_CONFIG_VERSION = aktVersion;
