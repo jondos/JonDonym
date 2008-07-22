40be84adef8edfd326ca5ceec9d0acfeffdb85db
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-07-22 14:49:42 +0000

    removed newline : )

--- src/anon/util/ProgressCapsule.java
+++ src/anon/util/ProgressCapsule.java
@@ -25,7 +25,6 @@ package anon.util;
 
 public interface ProgressCapsule {
 
-	
 	public final static int PROGRESS_FINISHED = 0;
 	public final static int PROGRESS_ONGOING = 1;
 	public final static int PROGRESS_ABORTED = 2;
