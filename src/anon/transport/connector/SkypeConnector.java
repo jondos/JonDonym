09addf266ee8de3d96ba0af46e75f089dfde78dd
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-09-04 07:53:42 +0000

    Again a DD beta release (00.10.022); see https://www.jondos.de/en/download/betaversion/changelog

--- src/anon/transport/connector/SkypeConnector.java
+++ src/anon/transport/connector/SkypeConnector.java
@@ -53,74 +53,95 @@ import com.skype.Stream;
  * <p>
  * Die zurueckgegeben Verbindung ist dabei Strom basierend.
  */
-public class SkypeConnector implements IConnector {
+public class SkypeConnector implements IConnector
+	{
 
-	/**
-	 * Versucht eine {@link IStreamConnection} zur angebenen Adresse aufzubauen.
-	 * <p>
-	 * Die gegebenfalls geworfenen Ausnahme, gibt genauere Hinweise, an welcher
-	 * Stelle Probleme mit dem Einrichten der Verbindung auftraten.
-	 * 
-	 * @param a_address
-	 *            Die Adresse wohin eine Verbindung aufgebaut werden soll.
-	 */
-	public IStreamConnection connect(SkypeAddress a_address)
-			throws ConnectionException {
-		Application app;
+		/**
+		 * Versucht eine {@link IStreamConnection} zur angebenen Adresse aufzubauen.
+		 * <p>
+		 * Die gegebenfalls geworfenen Ausnahme, gibt genauere Hinweise, an welcher
+		 * Stelle Probleme mit dem Einrichten der Verbindung auftraten.
+		 * 
+		 * @param a_address
+		 *          Die Adresse wohin eine Verbindung aufgebaut werden soll.
+		 */
+		public IStreamConnection connect(SkypeAddress a_address)
+				throws ConnectionException
+			{
+				Application app=null;
+				LogHolder.log(LogLevel.DEBUG, LogType.NET,"Skye Connector - Skype.setDaemon()");
+				Skype.setDeamon(false);
+				try
+					{
+						LogHolder.log(LogLevel.DEBUG, LogType.NET,"Skye Connector - Skype.setDebug()");
+						Skype.setDebug(true);
+						LogHolder.log(LogLevel.DEBUG, LogType.NET,"Skye Connector - Skype.setDebug - finished()");
+										}
+				catch (SkypeException e1)
+					{
+						LogHolder.log(LogLevel.DEBUG, LogType.NET,"Skye Connector - exception");
+					}
+				// are we trying to connect to ourself?
+				LogHolder.log(LogLevel.DEBUG, LogType.NET,"Skye Connector - try to get user id");
 
-		// are we trying to connect to ourself?
-		try {
-			String localID = Skype.getProfile().getId();
-			if (localID.equals(a_address.getUserID()))
-				throw new CommunicationException(
-						"No selfconection over Skype allowed");
-		} catch (SkypeException e) {
-			LogHolder.log(LogLevel.WARNING, LogType.TRANSPORT,
-					"Unable to get local Skype User ID");
-		}
+				try
+					{
+						String localID = Skype.getProfile().getId();
+						if (localID.equals(a_address.getUserID())) throw new CommunicationException(
+								"No selfconection over Skype allowed");
+					}
+				catch (SkypeException e)
+					{
+						LogHolder.log(LogLevel.WARNING, LogType.TRANSPORT,
+								"Unable to get local Skype User ID");
+					}
 
-		// so we try to register the application
-		try {
-			app = Skype.addApplication(a_address.getApplicationName());
-		} catch (SkypeException e) {
-			throw new CommunicationException(
-					"Unable to create desired Skype Application "
-							+ a_address.getApplicationName());
-		}
+				// so we try to register the application
+				LogHolder.log(LogLevel.DEBUG, LogType.TRANSPORT,"Try to register Skype forwarding application");
+				try
+					{
+						app = Skype.addApplication(a_address.getApplicationName());
+					}
+				catch (SkypeException e)
+					{
+						throw new CommunicationException(
+								"Unable to create desired Skype Application "
+										+ a_address.getApplicationName());
+					}
 
-		// did we got the application
-		if (app == null)
-			throw new CommunicationException(
-					"Unable to create desired Skype Application "
-							+ a_address.getApplicationName());
+				// did we got the application
+				if (app == null) throw new CommunicationException(
+						"Unable to create desired Skype Application "
+								+ a_address.getApplicationName());
 
-		// so we try to get an stream
-		Stream[] connectionStreams;
-		try {
-			connectionStreams = app.connect(new String[] { a_address
-					.getUserID() });
-		} catch (SkypeException e) {
-			throw new CommunicationException(
-					"Unable to connect to User with ID "
-							+ a_address.getUserID());
-		}
-		if ((connectionStreams == null) || (connectionStreams.length == 0))
-			throw new CommunicationException(
-					"Unable to connect to User with ID "
-							+ a_address.getUserID());
-		// we got at least on stream. let's build the base chunk connection
-		IChunkConnection baseConnection = new SkypeConnection(
-				connectionStreams[0]);
+				// so we try to get an stream
+				LogHolder.log(LogLevel.DEBUG, LogType.TRANSPORT,"Try to get a stream from Skype");
+				Stream[] connectionStreams=null;
+				try
+					{
+						connectionStreams = app.connect(new String[] { a_address.getUserID() });
+					}
+				catch (SkypeException e)
+					{
+						throw new CommunicationException(
+								"Unable to connect to User with ID " + a_address.getUserID());
+					}
+				if ((connectionStreams == null) || (connectionStreams.length == 0)) throw new CommunicationException(
+						"Unable to connect to User with ID " + a_address.getUserID());
+				// we got at least on stream. let's build the base chunk connection
+				LogHolder.log(LogLevel.DEBUG, LogType.TRANSPORT,"Setup the base Skype connection");
+				IChunkConnection baseConnection = new SkypeConnection(
+						connectionStreams[0]);
 
-		// an make an StreamConnection out of it
-		return new ChunkConnectionAdapter(baseConnection);
+				// and make an StreamConnection out of it
+				return new ChunkConnectionAdapter(baseConnection);
+			}
+
+		public IConnection connect(IAddress a_address) throws ConnectionException
+			{
+				if (!(a_address instanceof SkypeAddress)) throw new IllegalArgumentException(
+						"Connector can only handel Address of type SkypeAddress");
+				return connect((SkypeAddress) a_address);
+			}
+
 	}
-
-	public IConnection connect(IAddress a_address) throws ConnectionException {
-		if (!(a_address instanceof SkypeAddress))
-			throw new IllegalArgumentException(
-					"Connector can only handel Address of type SkypeAddress");
-		return connect((SkypeAddress) a_address);
-	}
-
-}
--- src/jap/JAPConstants.java
+++ src/jap/JAPConstants.java
@@ -35,11 +35,11 @@ import anon.mixminion.Mixminion;
 
 public final class JAPConstants
 {
-	public static final String aktVersion = "00.10.021"; //Never change the layout of this line!
-	private static final String CVS_GENERATED_RELEASE_DATE = "$Date: 2008-09-02 12:12:42 $";
+	public static final String aktVersion = "00.10.022"; //Never change the layout of this line!
+	private static final String CVS_GENERATED_RELEASE_DATE = "$Date: 2008-09-03 12:31:14 $";
 
 	//Warning: This is a little bit tricky,
-	//because CVS will expand the $Date: 2008-09-02 12:12:42 $
+	//because CVS will expand the $Date: 2008-09-03 12:31:14 $
 	//to the date of the last commmit of this file
 
 	//Never change the layout of the next two lines as serveal automated tools use these lines to do fancy things...
