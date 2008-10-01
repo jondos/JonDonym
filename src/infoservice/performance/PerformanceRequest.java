2483f9d7a167c1bab7229c429c9f2e29d6401249
cbanse <cbanse@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-10-01 11:56:54 +0000

    documentation

--- src/infoservice/performance/PerformanceRequest.java
+++ src/infoservice/performance/PerformanceRequest.java
@@ -1,3 +1,31 @@
+/*
+ Copyright (c) 2008 The JAP-Team, JonDos GmbH
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation and/or
+       other materials provided with the distribution.
+    * Neither the name of the University of Technology Dresden, Germany, nor the name of
+       the JonDos GmbH, nor the names of their contributors may be used to endorse or
+       promote products derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package infoservice.performance;
 
 import org.w3c.dom.Document;
@@ -10,28 +38,64 @@ import anon.util.IXMLEncodable;
 import anon.util.XMLParseException;
 import anon.util.XMLUtil;
 
+/**
+ * Java representation of a performance evaluation request.<br />
+ * <br />
+ * An inquiring <code>InfoService</code> should send a <code>PerformanceRequest</code>
+ * to another info service (or itself) to request random data
+ * of required size. This requires a valid <code>PerformanceToken</code>.
+ * 
+ * @see infoservice.performance.PerformanceToken
+ * 
+ * @author Christian Banse
+ */
 public class PerformanceRequest implements IXMLEncodable 
 {
+	/**
+	 * The XML element name
+	 */
 	public static final String XML_ELEMENT_NAME = "PerformanceRequest";
+	
+	/**
+	 * The token id XML element name
+	 */
 	public static final String XML_ELEMENT_TOKEN_ID = "TokenId";
+	
+	/**
+	 * The data size XML element name
+	 */
 	public static final String XML_ELEMENT_DATASIZE = "DataSize";
 	
+	/**
+	 * The token id.
+	 */
 	private String m_tokenId;
+	
+	/**
+	 * The data size.
+	 */
 	private int m_dataSize;
-		
+	
+	/**
+	 * Constructs a new <code>PeformanceRequest</code> object from XML data.
+	 * 
+	 * @param a_node The XML node.
+	 * @throws XMLParseException If the XML data or the token id was invalid.
+	 */
 	public PerformanceRequest(Element a_node) throws XMLParseException
 	{
+		// check if the element's name matches the required name
 		XMLUtil.assertNodeName(a_node, XML_ELEMENT_NAME);
 		
+		// check if it has a valid info service signature
 		if(!SignatureVerifier.getInstance().verifyXml(a_node, SignatureVerifier.DOCUMENT_CLASS_INFOSERVICE))
 		{
 			throw new XMLParseException("Could not verify XML Document.");
 		}
 		
+		// extract the tokenId
 		Node nodeToken = XMLUtil.getFirstChildByName(a_node, XML_ELEMENT_TOKEN_ID);
-		
 		XMLUtil.assertNotNull(nodeToken);
-		
 		m_tokenId = XMLUtil.parseValue(nodeToken, "");
 		
 		// check against available tokens
@@ -41,20 +105,29 @@ public class PerformanceRequest implements IXMLEncodable
 		{
 			throw new XMLParseException("Request did not specify a valid token.");
 		}
-		
+
+		// extract the data size
 		Node nodeDataSize = XMLUtil.getFirstChildByName(a_node, XML_ELEMENT_DATASIZE);
-		
 		XMLUtil.assertNotNull(nodeDataSize);
-		
 		m_dataSize = XMLUtil.parseValue(nodeDataSize, -1);		
 	}
 	
+	/**
+	 * Constructs a new <code>PerformanceRequest</code> object 
+	 * from a valid token id and dataSize
+	 * 
+	 * @param a_tokenId A valid token id.
+	 * @param a_dataSize The required data size.
+	 */
 	public PerformanceRequest(String a_tokenId, int a_dataSize)
 	{
 		m_tokenId = a_tokenId;
 		m_dataSize = a_dataSize;
 	}
 	
+	/**
+	 * Generates the XML representation of the <code>PerformanceRequest</code> object
+	 */
 	public Element toXmlElement(Document a_doc) 
 	{
 		Element elem = a_doc.createElement(XML_ELEMENT_NAME);
@@ -71,11 +144,21 @@ public class PerformanceRequest implements IXMLEncodable
 		return elem;
 	}
 	
+	/**
+	 * Returns the token id used by the request.
+	 * 
+	 * @return The token id used by the request.
+	 */
 	public String getTokenId()
 	{
 		return m_tokenId;
 	}
 	
+	/**
+	 * Returns the data size of the request.
+	 * 
+	 * @return The data size of the request.
+	 */
 	public int getDataSize()
 	{
 		return m_dataSize;
--- src/infoservice/performance/PerformanceRequestHandler.java
+++ src/infoservice/performance/PerformanceRequestHandler.java
@@ -1,7 +1,34 @@
+/*
+ Copyright (c) 2008 The JAP-Team, JonDos GmbH
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation and/or
+       other materials provided with the distribution.
+    * Neither the name of the University of Technology Dresden, Germany, nor the name of
+       the JonDos GmbH, nor the names of their contributors may be used to endorse or
+       promote products derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package infoservice.performance;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
 import infoservice.HttpResponseStructure;
 import logging.LogHolder;
@@ -12,8 +39,21 @@ import anon.util.XMLParseException;
 import anon.infoservice.Database;
 import anon.crypto.SignatureVerifier;
 
+/**
+ * Handles an incoming <code>PerformanceRequest</code> or
+ * <code>PerformanceTokenRequest</code>.
+ * 
+ * @author Christian Banse
+ */
 public class PerformanceRequestHandler 
 {
+	/**
+	 * Retrieves the XML structure of a PerformanceTokenRequest and
+	 * issues a new token to the <code>InfoService</code>.
+	 * 
+	 * @param a_postData The XML data.
+	 * @return A <code>HttpResponseStructure</code> with the issued token.
+	 */
 	public HttpResponseStructure handlePerformanceTokenRequest(byte[] a_postData)
 	{
 		Document doc = null;
@@ -28,15 +68,16 @@ public class PerformanceRequestHandler
 		{
 			LogHolder.log(LogLevel.WARNING, LogType.NET, "Error while processing PerformanceTokenRequest: " + ex.getMessage());
 			
-			// TODO: Return error?
 			return new HttpResponseStructure(HttpResponseStructure.HTTP_RETURN_BAD_REQUEST);
 		}
 		
 		LogHolder.log(LogLevel.WARNING, LogType.NET, "InfoService " + request.getInfoServiceId() + " is requesting a performance token.");
 	
+		// generate a new token
 		PerformanceToken token = new PerformanceToken();
 		Database.getInstance(PerformanceToken.class).update(token);
 		
+		// sign the token
 		doc = XMLUtil.toSignedXMLDocument(token, SignatureVerifier.DOCUMENT_CLASS_INFOSERVICE);
 		
 		HttpResponseStructure httpResponse = new HttpResponseStructure(HttpResponseStructure.HTTP_TYPE_TEXT_PLAIN,
@@ -47,6 +88,15 @@ public class PerformanceRequestHandler
 		return httpResponse;
 	}
 	
+	/**
+	 * Retrieves the XML structure of a PerformanceRequest and
+	 * sends random data back to the <code>InfoService</code>.
+	 * 
+	 * This requires a valid token id.
+	 * 
+	 * @param a_postData The XML data.
+	 * @return A <code>HttpResponseStructure</code> with the issued token.
+	 */
 	public HttpResponseStructure handlePerformanceRequest(byte[] a_postData)
 	{
 		Document doc = null;
@@ -55,26 +105,31 @@ public class PerformanceRequestHandler
 		try
 		{
 			doc = XMLUtil.toXMLDocument(a_postData);
+			
+			// the PerformanceRequest constructor will throw an exception
+			// if the info service did not specify a valid token id
 			request = new PerformanceRequest(doc.getDocumentElement());
 		}
 		catch(XMLParseException ex)
 		{
 			LogHolder.log(LogLevel.WARNING, LogType.NET, "Error while processing PerformanceTokenRequest: " + ex.getMessage());
 			
-			// TODO: Return error?
 			return new HttpResponseStructure(HttpResponseStructure.HTTP_RETURN_BAD_REQUEST);
 		}
 		
+		// we seem to have a valid token
 		LogHolder.log(LogLevel.WARNING, LogType.NET, "Token " + request.getTokenId() + " is requesting " + request.getDataSize() + " bytes of random data.");
 		
+		// generate random data
 		byte[] data = new byte[request.getDataSize()];
 		new java.util.Random().nextBytes(data);
 		
 		HttpResponseStructure httpResponse = new HttpResponseStructure(HttpResponseStructure.HTTP_TYPE_TEXT_PLAIN,
 				HttpResponseStructure.HTTP_ENCODING_PLAIN, data);
-
+		
 		LogHolder.log(LogLevel.WARNING, LogType.NET, data.length + " bytes sent. Removed token.");
 		
+		// remove the token from the database
 		Database.getInstance(PerformanceToken.class).remove(request.getTokenId());
 		
 		return httpResponse;
