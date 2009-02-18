586832bed2e4a760b3ab2944a5c1aa1a0855e86f
rolf <rolf@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2009-02-18 15:49:09 +0000

    fixed small cipher error

--- JonDonym/trunk/src/anon/client/crypto/DefaultMixCipher.java
+++ JonDonym/trunk/src/anon/client/crypto/DefaultMixCipher.java
@@ -126,7 +126,7 @@ public class DefaultMixCipher implements IMixCipher {
       encryptedPacket = new byte[packet.length];
     }
     /* do symmetric encryption */
-    m_symCipher.encryptAES1(packet, alreadyEncryptedBytes, encryptedPacket, alreadyOutputBytes, a_packet.length - alreadyEncryptedBytes);
+    m_symCipher.encryptAES1(packet, alreadyEncryptedBytes, encryptedPacket, alreadyOutputBytes, packet.length - alreadyEncryptedBytes);
     if (realPacketLength < encryptedPacket.length) {
       /* cut off the dummy bytes */
       byte[] tempPacket = encryptedPacket;
