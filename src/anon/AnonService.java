f9fb42de2d61a54d11c034cbcc2b4299af78730e
rolf <rolf@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2009-03-26 21:30:43 +0000

    - lower granularity on version statistics - added explicit custom filter option for free services

--- src/anon/AnonService.java
+++ src/anon/AnonService.java
@@ -37,7 +37,7 @@ import anon.infoservice.IMutableProxyInterface;
 public interface AnonService
 {
 	///The version of the AnonLib
-	String ANONLIB_VERSION = "00.11.010";
+	String ANONLIB_VERSION = "00.11.011";
 
 	/** Initializes this AnonService. Depending on the AnonService, this may establish a connection to
 	 *  an AnonServer, which is described through the
