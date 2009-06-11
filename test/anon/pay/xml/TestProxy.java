460474ac9ae7904258d1b97e2d672134cac0379d
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2009-06-11 13:39:01 +0000

    1. refactored XMLEasyCC for the sake of better price-cert hash concatenation testing. 2. re-enabled old format concatenation. 3. added a test case for old format concatenation.

---
+++ test/anon/pay/xml/TestProxy.java
@@ -0,0 +1,4 @@
+package anon.pay.xml;
+
+public class TestProxy extends junitx.framework.extension.TestProxy 
+{}
--- test/anon/pay/xml/XMLEasyCCTest.java
+++ test/anon/pay/xml/XMLEasyCCTest.java
@@ -1,5 +1,6 @@
 package anon.pay.xml;
 
+import java.util.Hashtable;
 import java.util.Properties;
 
 import junit.framework.Test;
@@ -11,45 +12,68 @@ import org.w3c.dom.Document;
 import anon.util.XMLUtil;
 
 /**
- * This compares the correctness of the price cert hash concatenation
+ * This compares the correctness of the price certificate hash concatenation
  * @author simon
  *
  */
 public class XMLEasyCCTest extends XtendedPrivateTestCase
 {	
-	final static int NR_OF_CCS = 13;
-	
-	public XMLEasyCCTest()
-	{
-		super("testHash");
-	}
+	private final static int NR_OF_CCS = 13;
+	private final static String PROPS_EXPECTED_RESULTS_OLD_FORMAT = 
+		"expectedOldFormat.properties";
+	private final static String PROPS_EXPECTED_RESULTS_NEW_FORMAT = 
+		"expectedNewFormat.properties";
+	private final static String CONCAT_METHOD_KEY = 
+		"createConcatenatedPriceCertHashes_"+Hashtable.class.getName()+"_boolean";
 	
 	public XMLEasyCCTest(String name)
 	{
 		super(name);
 	}
 
-	public void testHash() throws Exception
+	public void testHashNewFormat() throws Exception
+	{
+		doHashTestRun(true);
+	}
+	
+	public void testHashOldFormat() throws Exception
+	{
+		doHashTestRun(false);
+	}
+	
+	private void doHashTestRun(boolean newFormat) throws Exception
 	{
 		Properties expectedResults = new Properties();
-		expectedResults.load(this.getClass().getResourceAsStream("res/expected.properties"));
+		expectedResults.load(this.getClass().getResourceAsStream("res/"+
+				(newFormat ? PROPS_EXPECTED_RESULTS_NEW_FORMAT : 
+							PROPS_EXPECTED_RESULTS_OLD_FORMAT) ));
 		
 		String currentCC = null;
 		for (int i = 1; i <= NR_OF_CCS; i++)
 		{
 			currentCC = "CC"+i;
-			Document doc = XMLUtil.readXMLDocument(this.getClass().getResourceAsStream("res/"+currentCC+".xml"));
-			XMLEasyCC easyCC = new XMLEasyCC(doc.getDocumentElement());
+			Document doc = 
+				XMLUtil.readXMLDocument(
+						this.getClass().getResourceAsStream("res/"+currentCC+".xml"));
+			
+			
+			XMLEasyCC testProxy = new XMLEasyCC(doc.getDocumentElement());
+			
+			Object concatenatedHashResult = 
+				invokeWithKey(testProxy, CONCAT_METHOD_KEY , 
+					new Object[]{testProxy.getPriceCertHashes(), new Boolean(newFormat)});
+			
 			assertEquals("Check of "+currentCC, 
 					expectedResults.get(currentCC), 
-					easyCC.getConcatenatedPriceCertHashes());
+					concatenatedHashResult);
 		}
 	}
 	
 	public static Test suite()
 	{
-		TestSuite suite = new TestSuite();
-		suite.addTest(new XMLEasyCCTest());
+		TestSuite suite = new TestSuite(XMLEasyCC.class.getSimpleName());
+		suite.addTest(new XMLEasyCCTest("testHashNewFormat"));
+		suite.addTest(new XMLEasyCCTest("testHashOldFormat"));
 		return suite;
 	}
 }
